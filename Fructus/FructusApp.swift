4a3b8271541f7617008b76835b57bc90532afaaf
JC Manikis <jmanikis@icloud.com>
2021-12-29 15:45:32 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by JC Manikis on 2021-12-29.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
